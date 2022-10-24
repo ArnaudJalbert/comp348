0bea094495043cb414a8b470729645592fa085fd
ArnaudJalbert <74669399+ArnaudJalbert@users.noreply.github.com>
2022-10-24 19:27:47 -0400

    working for all words, not case sensitive anymore, just need to make the logger

--- C/assignment1/replace.c
+++ C/assignment1/replace.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include "replace.h"
 #include "traversal.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-    traversal(".", "short");
+    int status = 0;
+    if (argc == 2)
+    {
+        printf("----------STARTING TRAVERSAL------------\n");
+        char *word = malloc(strlen(argv[1]) * sizeof(char));
+        word = argv[1];
 
-    return 0;
+        status = traversal(".", word);
+        printf("----------TRAVERSAL FINISHED------------\n");
+    }
+    else if (argc == 1)
+    {
+        printf("No arguments were supplied, please supply a word to identify.\n");
+    }
+    else
+    {
+        printf("Too much arguments were supplied, please supply ONE word to identify.\n");
+    }
+
+    return status;
 }
